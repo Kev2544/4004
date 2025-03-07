b64f3ce6f9f119a99bc9d2b76e63c82fb43b118c
Kev2544 <kevyn.carrillo25@gmail.com>
2025-03-06 20:44:42 -0500

    Added synthesis run log files

---
+++ syn_runs/04_03_25/genus.cmd
@@ -0,0 +1,23 @@
+# Cadence Genus(TM) Synthesis Solution, Version 20.11-s111_1, built Apr 26 2021 14:57:38
+
+# Date: Wed Mar 05 02:51:07 2025
+# Host: ip-10-16-10-154.rdius.us (x86_64 w/Linux 3.10.0-1160.119.1.el7.x86_64) (2cores*4cpus*1physical cpu*Intel(R) Xeon(R) Platinum 8259CL CPU @ 2.50GHz 36608KB)
+# OS:   Red Hat Enterprise Linux Server release 7.9 (Maipo)
+
+source ../../syn/Script_syn_mcs4_genus.tcl
+# entering suspend mode
+resume
+# resuming normal operation
+# entering suspend mode
+resume
+# resuming normal operation
+# entering suspend mode
+resume
+# resuming normal operation
+# entering suspend mode
+syn_map
+syn_opt
+report_qor
+report_units
+report_timing_summary
+exit
