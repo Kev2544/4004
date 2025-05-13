b42db27324010e6428ce5b564507ee57c4219564
Vazquez Vega <roberto.vazquez.vega@intel.com>
2025-05-13 17:59:30 -0600

    vazquezv: change for io_pad connection x5

--- rtl/verilog/i4001/i4001.v
+++ rtl/verilog/i4001/i4001.v
@@ -267,10 +267,10 @@ module i4001 #(
                 else                assign io_pad_int[p] =  io_out[p];
             end
             else begin: IO_IN_CONFIG
-                assign io_pad_int[p] = 1'b0;
+                assign io_pad_int[p] = 1'bz;
                 if (IO_PULLUP[p])   assign io_pad_int[p] = 1'b1;
-                else if (IO_PULLDOWN[p]) assign io_pad_int = 1'b1;
-				else assign io_pad_int = io_pad_int;
+                else if (IO_PULLDOWN[p]) assign io_pad_int[p] = 1'b0;
+				else assign io_pad_int[p] = 1'bz;
             end
             if (IO_INVERT[p])   assign io_in[p] = ~io_pad_int[p];
             else                assign io_in[p] =  io_pad_int[p];
