1035e3a96fff27c9d583f71865d72fba7c6a37e4
Vazquez Vega <roberto.vazquez.vega@intel.com>
2025-05-13 21:49:16 -0600

    vazquezv: ???

--- syn/mcs4_constraints_slow.sdc.tcl
+++ syn/mcs4_constraints_slow.sdc.tcl
@@ -1,21 +1,21 @@
 # ITESO University
 # Kevyn Carrillo kevyn.carrillo@iteso.mx
 # Roberto Vazquez roberto.vazquezv@iteso.mx
-# User Constraint File: mcs4_constraints.sdc
-# Constraints file for Genus and GPDK045 Cadence PDK
+# User Constraint File: mcs4_constraints_slow.sdc
+# Constraints file for Genus and GPDK045 Cadence PDK - SLOW CORNER
 
 # Genus COMMANDS TO SET UNITS
 set_time_unit -nanoseconds
 set_load_unit -femtofarads
 
-# COMMON VARIABLES 
+# COMMON VARIABLES
 set EXTCLK "20MHz_CLK"
 set EXTCLK_PERIOD 50.0
-set SKEW 2.5  ;# Adjusted to 5% of clock period
-set MINRISE 0.5 ;# Adjusted to 1% of clock period
-set MAXRISE 2.5 ;# Adjusted to 5% of clock period
-set MINFALL 0.5 ;# Adjusted to 1% of clock period
-set MAXFALL 2.5 ;# Adjusted to 5% of clock period
+set SKEW 5.0  ;# Adjusted to 10% of clock period (slower)
+set MINRISE 1.0 ;# Adjusted to 2% of clock period (slower)
+set MAXRISE 5.0 ;# Adjusted to 10% of clock period (slower)
+set MINFALL 1.0 ;# Adjusted to 2% of clock period (slower)
+set MAXFALL 5.0 ;# Adjusted to 10% of clock period (slower)
 
 # Clock definition
 create_clock -name $EXTCLK -period $EXTCLK_PERIOD -waveform {0 25} [get_ports sysclk]
@@ -27,23 +27,23 @@ set_clock_transition -fall -max $MAXFALL [get_clocks $EXTCLK]
 
 # TO DEFINE CLOCK SOURCE AND NETWORK LATENCY IN GENUS
 # Define waveform settings for Source Latency
-set_clock_latency -rise -source 2.5 -early -late $EXTCLK  ;# Adjusted to 5% of clock period
-set_clock_latency -fall -source 2.5 -early -late $EXTCLK  ;# Adjusted to 5% of clock period
+set_clock_latency -rise -source 5.0 -early -late $EXTCLK  ;# Adjusted to 10% of clock period (slower)
+set_clock_latency -fall -source 5.0 -early -late $EXTCLK  ;# Adjusted to 10% of clock period (slower)
 # Define waveform settings for Network Latency
-set_clock_latency -rise 2.5 $EXTCLK  ;# Adjusted to 5% of clock period
-set_clock_latency -fall 2.5 $EXTCLK  ;# Adjusted to 5% of clock period
+set_clock_latency -rise 5.0 $EXTCLK  ;# Adjusted to 10% of clock period (slower)
+set_clock_latency -fall 5.0 $EXTCLK  ;# Adjusted to 10% of clock period (slower)
 
 # CLOCK UNCERTAINTY
-set_clock_uncertainty -setup 2.5 [get_clocks $EXTCLK]  ;# Adjusted to 5% of clock period
-set_clock_uncertainty -hold  0.5 [get_clocks $EXTCLK]  ;# Adjusted to 1% of clock period
+set_clock_uncertainty -setup 5.0 [get_clocks $EXTCLK]  ;# Adjusted to 10% of clock period (slower)
+set_clock_uncertainty -hold  1.0 [get_clocks $EXTCLK]  ;# Adjusted to 2% of clock period (slower)
 
 # Input delay definition: This is the delay coming from outside the design
-# for this design it's defined at 10% the period of the clock.
-set_input_delay -clock [get_clocks $EXTCLK] -add_delay 5 -name I_DELAY  [get_ports -filter "(direction == in || direction == inout) && name != sysclk"]
+# for this design it's defined at 20% the period of the clock (slower).
+set_input_delay -clock [get_clocks $EXTCLK] -add_delay 10 -name I_DELAY  [get_ports -filter "(direction == in || direction == inout) && name != sysclk"]
 
 # Output delay definition: This is the delay going outside the design
-# for this design it's defined at 10% the period of the clock.
-set_output_delay -clock [get_clocks $EXTCLK] -add_delay 5 -name O_DELAY [get_ports -filter "direction == out || direction == inout"]
+# for this design it's defined at 20% the period of the clock (slower).
+set_output_delay -clock [get_clocks $EXTCLK] -add_delay 10 -name O_DELAY [get_ports -filter "direction == out || direction == inout"]
 
 # Driving cell definition
 # BUFx2 is a buffer of 2 drive strength of the slow_vdd1v0_basicCells library
@@ -53,7 +53,7 @@ set_max_capacitance 5 [all_outputs]
 
 set_max_fanout 15.000 [current_design]
 
-set_max_transition 20 [current_design]
+set_max_transition 40  ;# Slower transition
 
 # Alternative to external_pin_cap
 set_load 5 -pin_load [get_ports {p_out[*] io_pad[*]}]
